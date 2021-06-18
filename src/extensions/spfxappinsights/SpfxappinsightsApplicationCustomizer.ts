b8ef471ee6dc9e52a9345b535c613a2890bde753
Tomasz Wojtasik <twojtasik@it-dev.pl>
2021-06-18 17:06:58 +0200

    basic support for partial reloads

--- src/extensions/spfxappinsights/SpfxappinsightsApplicationCustomizer.ts
+++ src/extensions/spfxappinsights/SpfxappinsightsApplicationCustomizer.ts
@@ -24,40 +24,53 @@ export interface ISpfxappinsightsApplicationCustomizerProperties {
 export default class SpfxappinsightsApplicationCustomizer
   extends BaseApplicationCustomizer<ISpfxappinsightsApplicationCustomizerProperties> {
 
+    private _instrumentationKey:string;
+    private _appInsights:ApplicationInsights;
+
   @override
   public onInit(): Promise<void> {
-    let instrumentationKey: string = this.properties.instrumentationKey;
-    
-    if (instrumentationKey && instrumentationKey != KeyDefaultValue) {
-      return SPPageContextInfo.getContext().then(context => {
-        const appInsights = new ApplicationInsights({
-          config: {
-            instrumentationKey: instrumentationKey
-          }
-        });
+    this._instrumentationKey = this.properties.instrumentationKey;
 
-        appInsights.loadAppInsights();
-        appInsights.trackPageView({
-          properties: {
-            "document.referrer": document.referrer,
-            "sppagecontextinfo.currentCultureName": context.currentCultureName,
-            "sppagecontextinfo.currentUICultureName": context.currentUICultureName,
-            "sppagecontextinfo.isExternalGuestUser": context.isExternalGuestUser,
-            "sppagecontextinfo.isEmailAuthenticationGuestUser": context.isEmailAuthenticationGuestUser,
-            "sppagecontextinfo.isAnonymousGuestUser": context.isAnonymousGuestUser,
-            "sppagecontextinfo.isSiteOwner": context.isSiteOwner,
-            "sppagecontextinfo.isSiteAdmin": context.isSiteAdmin,
-            "sppagecontextinfo.webTemplateConfiguration": context.webTemplateConfiguration,
-            "sppagecontextinfo.webTitle": context.webTitle,
-            "sppagecontextinfo.webAbsoluteUrl": context.webAbsoluteUrl,
-            "sppagecontextinfo.siteAbsoluteUrl": context.siteAbsoluteUrl,
-            "sppagecontextinfo.listTitle": context.listTitle,
-            "snppagecontextinfo.isWebWelcomePage": context.isWebWelcomePage,
-          }
-        });
-      });
+    
+    if (this._instrumentationKey && this._instrumentationKey != KeyDefaultValue) {
+      
+      this.context.application.navigatedEvent.add(this, this.navigationEventHandler);      
     }
 
     return Promise.resolve();
+  }
+
+  private _trackPageViewWithContext(context: any) {
+    this._appInsights = new ApplicationInsights({
+      config: {
+        instrumentationKey: this._instrumentationKey
+      }
+    });
+
+    this._appInsights.loadAppInsights();
+    this._appInsights.trackPageView({
+      properties: {
+        "document.referrer": document.referrer,
+        "sppagecontextinfo.currentCultureName": context.currentCultureName,
+        "sppagecontextinfo.currentUICultureName": context.currentUICultureName,
+        "sppagecontextinfo.isExternalGuestUser": context.isExternalGuestUser,
+        "sppagecontextinfo.isEmailAuthenticationGuestUser": context.isEmailAuthenticationGuestUser,
+        "sppagecontextinfo.isAnonymousGuestUser": context.isAnonymousGuestUser,
+        "sppagecontextinfo.isSiteOwner": context.isSiteOwner,
+        "sppagecontextinfo.isSiteAdmin": context.isSiteAdmin,
+        "sppagecontextinfo.webTemplateConfiguration": context.webTemplateConfiguration,
+        "sppagecontextinfo.webTitle": context.webTitle,
+        "sppagecontextinfo.webAbsoluteUrl": context.webAbsoluteUrl,
+        "sppagecontextinfo.siteAbsoluteUrl": context.siteAbsoluteUrl,
+        "sppagecontextinfo.listTitle": context.listTitle,
+        "snppagecontextinfo.isWebWelcomePage": context.isWebWelcomePage,
+      }
+    });
+  }
+
+  private navigationEventHandler(): void{
+    SPPageContextInfo.getContext().then(context => {        
+      this._trackPageViewWithContext(context);
+    });
   }
 }
